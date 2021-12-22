f9ec0f16c854c88ec288a19af69552bfde4ee440
aerocyber <86760749+AA-5@users.noreply.github.com>
2021-12-22 15:36:21 +0530

    Add envCreate.py and install.py

---
+++ crossapp/envCreate.py
@@ -0,0 +1,49 @@
+# Copyright 2021 aerocyber
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#### About ####
+# This script is used to create the virtual environment with which Apps are to be used.
+# This acts as an isolated Python so as not to interfere with the existing Python.
+# This, however does not downloads a new Python interpreter.
+#### End ####
+
+#### Imports ####
+import os
+import venv
+#### End ####
+
+#### Classes ####
+
+class createEnv:
+    """
+    Create the virtual environment with which Apps are to be used.
+    """
+    def __init__(self, Path_to_env):
+        """
+        Initialize the class with the name of the application.
+        """
+        self.path = Path_to_env
+    def create_env(self):
+        """
+           Use Python's venv module for environment creation.
+        """
+        if not os.path.exists(self.path):
+            venv.create(
+                os.path.normpath(os.path.normcase(self.path)),
+                with_pip=False,
+                symlinks=False
+            )
+
+
+#### End ####
--- crossapp/install.py
+++ crossapp/install.py
@@ -1,18 +1,18 @@
 # Copyright 2021 aerocyber
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#### About #### 
+#### About ####
 # This script is used to install the application from repository or a .cap file.
 # It will create a virtualenv, if already not present, and install the application.
 # It will also create a database, if already not present.
@@ -21,6 +21,10 @@
 #### Imports ####
 import os
 import requests
+import envCreate
+import pickle
+import json
+import shutil
 #### End ####
 
 #### Constants ####
@@ -48,13 +52,16 @@ def create_crossapp_dir():
         os.makedirs(CROSSAPP_DIR)
 
 # Create virtualenv directory
+
+
 def create_virtualenv_dir():
     """
     Create the virtualenv directory if not exists already.
     """
     if not os.path.exists(VIRTUALENV_DIR):
-        os.makedirs(VIRTUALENV_DIR)
-    
+        env = envCreate(VIRTUALENV_DIR)
+        env.create_env()
+
 
 # Create database directory
 def create_database_dir():
@@ -65,6 +72,8 @@ def create_database_dir():
         os.makedirs(DATABASE_DIR)
 
 # Create database file
+
+
 def create_database_file():
     """
     Create the database file if not exists already.
@@ -76,14 +85,32 @@ def create_database_file():
 
 #### Classes ####
 
+# Error
+
+
+class AppNotFound(Exception):
+    """
+    Exception class for AppNotFound.
+    """
+    pass
+
+
+class AppAlreadyInstalled(Exception):
+    """
+    Exception class for AppAlreadyInstalled.
+    """
+    pass
+
 # Installation control.
+
+
 class Install:
     """
     Installation class.
     """
 
     # Initialize the installation.
-    def __init__(self, crossapp_dir = CROSSAPP_DIR, virtualenv_dir = VIRTUALENV_DIR, database_dir = DATABASE_DIR, database_file = DATABASE_FILE):
+    def __init__(self, crossapp_dir=CROSSAPP_DIR, virtualenv_dir=VIRTUALENV_DIR, database_dir=DATABASE_DIR, database_file=DATABASE_FILE):
         """
         Initialize the installation.
         """
@@ -95,10 +122,152 @@ class Install:
         create_database_dir()
         # Create database file
         create_database_file()
-    
+
+    def get_installed_apps(self):
+        """
+        Get the list of installed apps.
+        """
+        f = open(DATABASE_FILE, "rb")
+        db = pickle.load(f)
+        f.close()
+        return db  # Return the list of installed apps.
+
+    def remove_app(self, appid, removeDataDir=False):
+        """
+            Remove the application.
+        """
+        f = open(DATABASE_FILE, "rb")
+        db = pickle.load(f)
+        f.close()
+        if appid in db:
+            loc = db[appid]["Location"]
+        else:
+            return AppNotFound("App not installed.")
+
+        if removeDataDir:
+            shutil.rmtree(loc)
+            del db[appid]
+        else:
+            del db[appid]
+            # Delete the pyz file.
+            os.remove(os.path.join(loc, db[appid]["Name"] + '.pyz'))
+        f = open(DATABASE_FILE, "wb")
+        pickle.dump(db, f)
+        f.close()
+        return True  # Indicate successful operation.
+
     # Install the application from repository.
     def install_from_repo(self, appid, repo):
         """
-        Install the application from repository.
+           Install the application from repository.
+
+           Args:
+                appid (str): The application ID.
+                repo (str): The repository URL.
+
+            Returns:
+                bool: True if the installation is successful.
+
+            Exception:
+                AppNotFound: If the application is not found in repo.
         """
-        pass
+        repo_get = requests.get(repo)  # Get repository
+        repo_get.raise_for_status()  # Raise exception if error
+        if repo_get.status_code == 200:
+            # Continue.
+            apps = repo_get.json()  # Get the JSON data
+            if appid in apps:
+                # Continue.
+                app = apps[appid]
+            else:
+                # Raise exception.
+                raise AppNotFound(
+                    "Application not found in {repourl}".format(repourl=repo))
+            # Check if app is already installed.
+            apps_installed = self.get_installed_apps()
+            if appid in apps_installed:
+                if app["version"] == apps_installed[appid]["version"]:
+                    # App already installed.
+                    raise AppAlreadyInstalled("App already installed.")
+                else:
+                    # Remove already installed app.
+                    self.remove_app(appid)
+
+            # Install the application.
+            # Write the application's name to the database.
+            f = open(DATABASE_FILE, "rb")
+            db = pickle.load(f)
+            f.close()
+            db[appid] = {
+                "Name": app["name"],
+                "Repo": repo,
+                "Version": app["version"],
+                "Author": app["author"],
+                "Description": app["description"],
+                "Website": app["website"],
+                "License": app["license"],
+                "Location": os.path.join(CROSSAPP_DIR, "Apps", appid)
+            }
+            f = open(DATABASE_FILE, "wb")
+            pickle.dump(db, f)
+            f.close()
+            # Create the application's directory.
+            if not os.path.exists(os.path.join(CROSSAPP_DIR, "Apps", appid)):
+                os.makedirs(os.path.join(CROSSAPP_DIR, "Apps", appid))
+            # Write the application's code to the directory.
+            f = open(os.path.join(CROSSAPP_DIR, "Apps",
+                     appid, app["Name"] + '.pyz'), 'wb')
+            f.write(app["Data"])
+            f.close()
+            return True  # Indicate successful operation.
+
+    def install_from_cap(self, cap_file_path):
+        """
+            Install app from cap file.
+        Args:
+            cap_file_path (str): Path to cap file.
+        """
+        if not os.path.exists(cap_file_path):
+            raise FileNotFoundError("Cap file not found.")
+
+        # Read cap file.
+        with open(cap_file_path, "r") as f:
+            cap = json.load(f)
+
+        # Check if app is already installed.
+        installed_apps = self.get_installed_apps()
+        if cap["appid"] in installed_apps:
+            if cap["version"] == installed_apps[cap["appid"]]["version"]:
+                # App already installed.
+                raise AppAlreadyInstalled("App already installed.")
+            else:
+                # Remove already installed app.
+                self.remove_app(cap["appid"])
+
+        # Install the application.
+        # Write the application's name to the database.
+            f = open(DATABASE_FILE, "rb")
+            db = pickle.load(f)
+            f.close()
+            db[cap["appid"]] = {
+                "Name": cap["name"],
+                "Repo": cap["repo"],
+                "Version": cap["version"],
+                "Author": cap["author"],
+                "Description": cap["description"],
+                "Website": cap["website"],
+                "License": cap["license"],
+                "Location": os.path.join(CROSSAPP_DIR, "Apps", cap["appid"])
+            }
+            f = open(DATABASE_FILE, "wb")
+            pickle.dump(db, f)
+            f.close()
+            # Create the application's directory.
+            if not os.path.exists(os.path.join(CROSSAPP_DIR, "Apps", cap["appid"])):
+                os.makedirs(os.path.join(CROSSAPP_DIR, "Apps", cap["appid"]))
+            # Write the application's code to the directory.
+            f = open(os.path.join(CROSSAPP_DIR, "Apps",
+                     cap["appid"], cap["Name"] + '.pyz'), 'wb')
+            f.write(cap["Data"])
+            f.close()
+            return True  # Indicate successful operation.
